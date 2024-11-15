3ff5c43cc1162cc8dc1bd1598650c7fddbb365d8
TadaaLenard <lenard8794@gmail.com>
2024-11-15 18:05:46 +0800

    Done basic library method and input function

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,36 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Creating a book instance
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234567890");
-
-        // Displaying book details
-        book1.displayDetails();
-
-        // Borrowing the book
-        book1.borrowBook("Alice");
-
-        // Displaying book details again to see changes
-        book1.displayDetails();
-
-        // Trying to borrow the book again
-        book1.borrowBook("Bob");
-
-        // Returning the book
-        book1.returnBook();
-
-        // Displaying book details to confirm it's available
-        book1.displayDetails();
+    public static void main(String[] args) throws IOException {
+//        // Creating a book instance
+//        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234567890");
+//
+//        // Displaying book details
+//        book1.displayDetails();
+//
+//        // Borrowing the book
+//        book1.borrowBook("Alice");
+//
+//        // Displaying book details again to see changes
+//        book1.displayDetails();
+//
+//        // Trying to borrow the book again
+//        book1.borrowBook("Bob");
+//
+//        // Returning the book
+//        book1.returnBook();
+//
+//        // Displaying book details to confirm it's available
+//        book1.displayDetails();
 
         Library library = new Library();
         System.out.print("A library is created.\n");
-        library.add_book(book1);
-        System.out.print("book1 is added into the library\n");
 
-        library.search_book();
+        String filename = "src/Lib1.txt";
+        library.ReadLibrary(filename);
+
+
+        library.displayAllBook();
 
     }
 }
